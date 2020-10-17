70fcaa3ef88c02e1ce9dc924e08818600e0d977d
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-17 14:12:03 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,29 @@
+// print all num between -10 to 19
+
+for (let i = -10; i < 20; i++){
+console.log(i)
+}
+
+// print even numbers between 10 - 40
+
+for (let j = 10; j <= 40; j++){
+    if ( j % 2 === 0){
+    console.log(j)
+}
+}
+
+//print odd number between 300 to 333
+
+for (let k = 300; k <=333; k++){
+    if (k % 2 !== 0){
+        console.log(k)
+    }
+}
+
+// print out number divisible 5 and 3 between 5 to 50
+
+for (let m = 5; m <= 333; m++){
+    if (m % 3 === 0 && m % 5 === 0 ){
+        console.log(m)
+    }
+}
